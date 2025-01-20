a79e7281abb1042ae4610c87046bffe18f654672
Nitin Verma <nitinkumarv2021@gmail.com>
2025-01-20 12:41:25 +0530

    Fixed image path and updated other necessary code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"; 
 import HomePage from "./pages/HomePage";
 import RummyPage from "./pages/RummyPage";
 import ColorPage from "./pages/ColorPage";
@@ -8,12 +8,14 @@ import OtherPage from "./pages/OtherPage";
 function App() {
   return (
     <Router>
+      <Switch>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/rummy" element={<RummyPage />} />
         <Route path="/color" element={<ColorPage />} />
         <Route path="/other" element={<OtherPage/>} />
       </Routes>
+      </Switch>
     </Router>
   );
 }
